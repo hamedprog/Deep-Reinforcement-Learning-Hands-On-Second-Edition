352e54073dcac9099106779a1267095d046a42ad
hamed Prog <hamed0381@gmail.com>
2020-09-28 13:36:24 +0330

    print commented

--- Chapter03/01_modules.py
+++ Chapter03/01_modules.py
@@ -25,4 +25,5 @@ if __name__ == "__main__":
     print(out)
     print("Cuda's availability is %s" % torch.cuda.is_available())
     if torch.cuda.is_available():
-        print("Data from cuda: %s" % out.to('cuda'))
+        # print("Data from cuda: %s" % out.to('cuda'))
+
